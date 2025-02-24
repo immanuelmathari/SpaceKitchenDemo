d85ad998e2c4eb43ea336f934d7f1fcc3d419503
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-02-24 10:05:19 +0300

    6.100 Displaying Meals 24/02

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealOverviewScreen({ route })
 {
     const categoryId = route.params.categoryId;
+    const displayedMeals = MEALS.filter((meal) => {
+        return meal.categoryIds.indexOf(categoryId) >= 0;
+    })
+
+    // we define this function in here because we shall need a prop defined here
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
     return (
         <View style={styles.container}>
             <Text>Meals Overview Screen - {categoryId}</Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
         </View>
     )
 }
